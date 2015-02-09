d79eac2d8d991043c1edbe1123cd0eb025c5762c
Dmitry Zhukov <dmitry.zhukov@gmail.com>
2015-02-09 14:46:59 +0700

    scripts updated

--- pods_init.py
+++ pods_init.py
@@ -23,9 +23,9 @@ os.environ['DISCOVERY_URL'] = urllib2.urlopen("https://discovery.etcd.io/new").r
 os.environ['NUM_OF_DROPLETS'] = str(CLUSTER_SIZE)
 
 
+z = 0 - CLUSTER_SIZE
 kub_ip = client.droplets.list()[-1]["droplets"][z]["networks"]["v4"][0]["ip_address"]
 drupal_ip_list = list()
-z = 0 - CLUSTER_SIZE
 
 
 # copy and run deployment script of local docker registry on each node
