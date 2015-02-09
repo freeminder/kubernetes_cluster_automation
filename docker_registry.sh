fdd89150ca2f7bc3b02909bab3e25efe95863d79
Dmitry Zhukov <dmitry.zhukov@gmail.com>
2015-02-09 14:50:51 +0700

    scripts updated

--- docker_registry.sh
+++ docker_registry.sh
@@ -14,5 +14,5 @@ else
 	git clone https://github.com/freeminder/kubernetes_cluster_automation
 	mv kubernetes_cluster_automation/pods/myregistry1.yaml kubernetes_cluster_automation/pods/myregistry$HOST_ID.yaml && \
 	sed -i s/docker-registry1/docker-registry${HOST_ID}/ kubernetes_cluster_automation/pods/myregistry$HOST_ID.yaml && \
-	kubernetes_cluster_automation/kubecfg -h http://$KUB_IP:8080 -c kubernetes_cluster_automation/pods/myregistry$HOST_ID.yaml create pods/ && \
+	kubernetes_cluster_automation/kubecfg -h http://$KUB_IP:8080 -c kubernetes_cluster_automation/pods/myregistry$HOST_ID.yaml create pods/
 fi
