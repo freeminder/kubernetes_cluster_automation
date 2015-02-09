72b168ced4ce141cb3c8a829000bf94dad976198
Dmitry Zhukov <dmitry.zhukov@gmail.com>
2015-02-09 18:04:18 +0700

    scripts updated

--- destroy_script.sh
+++ destroy_script.sh
@@ -2,11 +2,12 @@
 KUB_IP=$1
 HOST_ID=$2
 
-cd ~
-kubernetes_cluster_automation/kubecfg -h http://$KUB_IP:8080 delete pods/drupal$HOST_ID
-kubernetes_cluster_automation/kubecfg -h http://$KUB_IP:8080 delete pods/docker-registry$HOST_ID
-sleep 5
-docker rmi -f drupal
-docker rmi -f localhost:5000/drupal
-sudo docker ps -a | grep Exit | awk '{print $1}' | sudo xargs docker rm
-docker rmi -f localhost:5000/drupal
+~/kubernetes_cluster_automation/kubecfg -h http://$KUB_IP:8080 delete pods/drupal$HOST_ID
+~/kubernetes_cluster_automation/kubecfg -h http://$KUB_IP:8080 delete pods/docker-registry$HOST_ID
+
+DOCKER_PS=`docker ps|grep drupal|wc -l`
+while [ "DOCKER_PS" != "0"]; do
+	docker rmi -f drupal
+	docker rmi -f localhost:5000/drupal
+	sudo docker ps -a | grep Exit | awk '{print $1}' | sudo xargs docker rm
+done
