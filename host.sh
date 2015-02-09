6b3f54f7d6691a7360715935f7b5be1fe3e91e8c
Dmitry Zhukov <dmitry.zhukov@gmail.com>
2015-02-09 13:58:12 +0700

    host.sh updated

--- host.sh
+++ host.sh
@@ -12,10 +12,14 @@ then
 	kubernetes_cluster_automation/kubecfg -h http://$KUB_IP:8080 -c kubernetes_cluster_automation/pods/myregistry1.yaml create pods/ && \
 	cd ~
 	rm -fr drupal_allin
-	sleep 60 && git clone https://github.com/freeminder/drupal_allin
+	sleep 20 && git clone https://github.com/freeminder/drupal_allin
 	docker build -t drupal drupal_allin && docker tag drupal localhost:5000/drupal && docker push localhost:5000/drupal
 	# create drupal pod
 	kubernetes_cluster_automation/kubecfg -h http://$KUB_IP:8080 -c kubernetes_cluster_automation/pods/drupal$HOST_ID.yaml create pods/
+	# patch drupal settings
+	sleep 20
+	DRUPAL_ID=`docker ps|grep drupal:latest|awk '{print $1}'`
+	sudo docker exec -i -t $DRUPAL_ID cp -f /var/www/sites/default/settings.php /var/www/sites/default/settings.orig
 else
 	# create local registry
 	cd ~
@@ -27,7 +31,7 @@ else
 	# replace IP of mysql master in drupal image; build, tag and push drupal image
 	cd ~
 	rm -fr drupal_allin
-	sleep 60 && git clone https://github.com/freeminder/drupal_allin
+	sleep 20 && git clone https://github.com/freeminder/drupal_allin
 	if [[ $MYSQL_MASTER == *"10"* ]]
 	then
 		sed -i s/gcomm\\:\\/\\//gcomm\\:\\/\\/${MYSQL_MASTER}/ drupal_allin/my.cnf;
@@ -37,4 +41,8 @@ else
 	mv kubernetes_cluster_automation/pods/drupal1.yaml kubernetes_cluster_automation/pods/drupal$HOST_ID.yaml
 	sed -i s/drupal1/drupal${HOST_ID}/ kubernetes_cluster_automation/pods/drupal$HOST_ID.yaml
 	kubernetes_cluster_automation/kubecfg -h http://$KUB_IP:8080 -c kubernetes_cluster_automation/pods/drupal$HOST_ID.yaml create pods/
+	# patch drupal settings
+	sleep 20
+	DRUPAL_ID=`docker ps|grep drupal:latest|awk '{print $1}'`
+	sudo docker exec -i -t $DRUPAL_ID cp -f /var/www/sites/default/settings.orig /var/www/sites/default/settings.php
 fi
