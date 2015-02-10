a1d73a992456815c440aa13e6f892dd04c84f2db
Dmitry Zhukov <dmitry.zhukov@gmail.com>
2015-02-10 20:14:25 +0700

    scripts updated

--- scripts/docker_drupal.sh
+++ scripts/docker_drupal.sh
@@ -48,5 +48,6 @@ DRUPAL_MASTER_CHECK=`docker ps|grep drupal1`
 DRUPAL_ID=`docker ps|grep drupal:latest|awk '{print $1}'`
 if [[ $DRUPAL_MASTER_CHECK != "" ]]
 then
+	sleep 20
 	sudo docker exec -i -t $DRUPAL_ID cp -f /var/www/sites/default/settings.php /var/www/sites/default/settings.orig
 fi
