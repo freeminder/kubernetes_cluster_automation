dec02ab47aac4df393852188f713dcce8747968c
Dmitry Zhukov <dmitry.zhukov@gmail.com>
2015-02-10 17:45:26 +0700

    scripts updated

--- scripts/destroy_script.sh
+++ scripts/destroy_script.sh
@@ -5,9 +5,15 @@ HOST_ID=$2
 ~/kubernetes_cluster_automation/bin/kubecfg -h http://$KUB_IP:8080 delete pods/drupal$HOST_ID
 ~/kubernetes_cluster_automation/bin/kubecfg -h http://$KUB_IP:8080 delete pods/docker-registry$HOST_ID
 
-DOCKER_PS=`docker ps|grep drupal:latest|wc -l`
-while [[ $DOCKER_PS != 0 ]]; do
+DRUPAL_IMAGES=`docker images|grep drupal|wc -l`
+while [[ $DRUPAL_IMAGES != 0 ]]; do
 	docker rmi -f drupal
 	docker rmi -f localhost:5000/drupal
-	sudo docker ps -a | grep Exit | awk '{print $1}' | sudo xargs docker rm
+	sudo docker ps -a | grep Exit | awk '{print $1}' | sudo xargs docker rm -f
 done
+
+# DOCKER_PS=`docker ps|grep drupal:latest|wc -l`
+# while [[ $DOCKER_PS != 0 ]]; do
+# 	docker rm -f $DOCKER_PS
+# 	sudo docker ps -a | grep Exit | awk '{print $1}' | sudo xargs docker rm -f
+# done
--- scripts/remove_pods.py
+++ scripts/remove_pods.py
@@ -5,6 +5,20 @@ import random, string, urllib, urllib2, fileinput, shutil, os, sys, time, re
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
 from myconfig import *
 
+# import subprocess
+# import os
+
+# processes = []
+# for file in files_output:
+#     f = os.tmpfile()
+#     p = subprocess.Popen(['md5sum',file],stdout=f)
+#     processes.append((p, f))
+
+# for p, f in processes:
+#     p.wait()
+#     f.seek(0)
+#     logfile.write(f.read())
+#     f.close()
 
 # set variables
 # dosa.set_debug()  # enables debug logs
